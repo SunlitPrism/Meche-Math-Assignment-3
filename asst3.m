29379070b84067b57ed085a9cd0972640537d0ec
SunlitPrism <rand.avery.mosley@gmail.com>
2025-10-03 16:15:03 -0400

    split

--- asst3.m
+++ asst3.m
@@ -1,37 +1,17 @@
 %This function computes the value of X at the next time step
 %using the Forward Euler approximation
 function asst3()
+    %define constants
+    t = 1;
+    h = 1; %timestep
+    X = [0,1]; %inital X
+    
+    dXdt = rate_func01(t,X);
+    XA = solution01(t);
 
-end
-%INPUTS:
-%rate_func_in: the function used to compute dXdt. rate_func_in will
-% have the form: dXdt = rate_func_in(t,X) (t is before X)
-%t: the value of time at the current step
-%XA: the value of X(t)
-%h: the time increment for a single step i.e. delta_t = t_{n+1} - t_{n}
-%OUTPUTS:
-%XB: the approximate value for X(t+h) (the next step)
-% formula depends on the integration method used
-%num_evals: A count of the number of times that you called
-% rate_func_in when computing the next step
-function [XB,num_evals] = forward_euler_step(rate_func_in,t,XA,h)
-%your code here
+    disp(dXdt)
+    
+
+    [XB,~] = forward_euler_step(rate_func01,t,X,h);
 end
 
-
-%% Function Implementation
-%First Function
-function dXdt = rate_func01(t,X)
-dXdt = -5*X + 5*cos(t) - sin(t);
-end
-function X = solution01(t)
-X = cos(t);
-end
-
-%Second Function
-function dXdt = rate_func02(t,X)
-dXdt = [0,-1;1,0]*X;
-end
-function X = solution02(t)
-X = [cos(t);sin(t)];
-end
