d43a654e5243ea44eff7ff3f242d4c54ffeac599
SunlitPrism <rand.avery.mosley@gmail.com>
2025-10-14 12:42:46 -0400

    Backward Euler Initialization

---
+++ backward_euler_step.m
@@ -0,0 +1,20 @@
+%This function computes the value of X at the next time step
+%using the Backward Euler approximation
+%INPUTS:
+%rate_func_in: the function used to compute dXdt. rate_func_in will
+% have the form: dXdt = rate_func_in(t,X) (t is before X)
+%t: the value of time at the current step
+%XA: the value of X(t)
+%h: the time increment for a single step i.e. delta_t = t_{n+1} - t_{n}
+%OUTPUTS:
+%XB: the approximate value for X(t+h) (the next step)
+% formula depends on the integration method used
+%num_evals: A count of the number of times that you called
+% rate_func_in when computing the next step
+function [XB,num_evals] = backward_euler_step(rate_func_in,t,XA,h)
+    dx = rate_func_in(t, XA);
+    Z = (1/(1+(dx*h)))*XA;
+    G = XA + h*rate_func_in(t + h, Z) - Z;
+    [XB, exit_flag] = multi_newton_asst3(G,XA,solver_params);
+    disp(XB)
+end
