c9173cb9a88f9ce20c4e40b5f3f229a07baea2dd
mayaadelman1 <madelman@olin.edu>
2025-10-16 21:31:55 -0400

    recent work

--- asst3.m
+++ asst3.m
@@ -78,72 +78,115 @@ function asst3()
     hold off
    
     
-    % Find local truncation error for Forward Euler, Explicit Midpoint, and Analytical Difference
-    [h_list, analytical_difference, fel_error_list,expmid_error_list] = truncation_error(t_ref, hspan,@rate_func01);
+    % Find local truncation error for Forward Euler, Explicit Midpoint, and Analytical Difference for test 1
+    [h_list, analytical_difference, fel_error_list, expmid_error_list, bel_error_list, impmid_error_list] = truncation_error(t_ref, hspan,@rate_func01);
     
     % Print out values for table of local truncation errors with step size
     % (values of p). p values chosen are the first and last values of
     % h_span, or -5 and -1.
-    fprintf("analytical_difference: ");
-    analytical_difference(1)
-    analytical_difference(end)
-    fprintf("fel_error_list: ")
-    fel_error_list(1)
-    fel_error_list(end)
-    fprintf("expmid_error_list: ")
-    expmid_error_list(1)
-    expmid_error_list(end)
+    % fprintf("analytical_difference: ");
+    % analytical_difference(1)
+    % analytical_difference(end)
+    % fprintf("fel_error_list: ")
+    % fel_error_list(1)
+    % fel_error_list(end)
+    % fprintf("expmid_error_list: ")
+    % expmid_error_list(1)
+    % expmid_error_list(end)
 
-    % Find line of best fit coefficients
+    % Find line of best fit coefficients and estimate p-value for test 1
+    [ana_p, ~] = loglog_fit(h_list, analytical_difference);
+    fprintf("Local analytical p-value: ");
+    disp(ana_p);
 
     [fel_p,fel_k] = loglog_fit(h_list,fel_error_list);
     fel_y_data = fel_k.*((h_list.^fel_p));
+    fprintf("Local forward Euler p-value: ");
+    disp(fel_p);
 
     [expmid_p,expmid_k] = loglog_fit(h_list,expmid_error_list);
     expmid_y_data = expmid_k.*((h_list.^expmid_p));
+    fprintf("Local explicit Midpoint p-value: ");
+    disp(expmid_p);
+
+    [bel_p, ~] = loglog_fit(h_list,bel_error_list);
+    fprintf("Local backward Euler p value: ");
+    disp(bel_p);
+
+    [impmid_p, ~] = loglog_fit(h_list,impmid_error_list);
+    fprintf("Local implicit Midpoint p-value: ");
+    disp(impmid_p);
+
+    % % Find local truncation error for Forward Euler, Explicit Midpoint, and Analytical Difference for test 2
+    % [h_list2, analytical_difference, fel_error_list2, expmid_error_list2, bel_error_list2, impmid_error_list2] = truncation_error(t_ref, hspan,@rate_func02);
+    % 
+    % % Estimate p-value for test 2
+    % [fel_p2, ~] = loglog_fit(h_list2,fel_error_list2);
+    % fprintf("Forward Euler p value: ");
+    % disp(fel_p2);
+    % 
+    % [expmid_p2,~] = loglog_fit(h_list2,expmid_error_list2);
+    % fprintf("Explicit Midpoint p-value: ");
+    % disp(expmid_p2);
+    % 
+    % [bel_p2, ~] = loglog_fit(h_list2,bel_error_list2);
+    % fprintf("Backward Euler p value: ");
+    % disp(bel_p2);
+    % 
+    % [impmid_p,~] = loglog_fit(h_list2,impmid_error_list2);
+    % fprintf("Implicit Midpoint p-value: ");
+    % disp(impmid_p);
 
 
     % Plot log scale graph of errors vs h_list (all the different step sizes used)
     figure;
 
     loglog(h_list,fel_error_list,'ro','MarkerFaceColor','r'); hold on;
-    loglog(h_list,fel_y_data,'k','LineWidth',2);
+    %loglog(h_list,fel_y_data,'k','LineWidth',2);
 
     loglog(h_list,expmid_error_list,'go','MarkerFaceColor','g');
-    loglog(h_list,expmid_y_data,'k','LineWidth',2); hold on;
+    %loglog(h_list,expmid_y_data,'k','LineWidth',2); hold on;
 
-    loglog(h_list,analytical_difference, 'bo', 'MarkerFaceColor', 'b'); hold off;
-    legend('Forward Euler','Fit line','Explicit Midpoint', '', 'Analytical Solution')
+    loglog(h_list,bel_error_list,'bo');
+    %loglog(h_list,bel_y_data,'k','LineWidth',2); hold on;
+
+    loglog(h_list,impmid_error_list,'mo');
+    %loglog(h_list,impmid_y_data,'k','LineWidth',2); hold off;
+
+    legend('Forward Euler', 'Backward Euler', 'Explicit Midpoint', 'Implicit Midpoint')
+    %loglog(h_list,analytical_difference, 'bo', 'MarkerFaceColor', 'b'); hold off;
+    %legend('Forward Euler','Fit line','Explicit Midpoint', '', 'Analytical Solution')
     % Set axes and legend
     xlim([1e-5 1e0])
-    title("Local error vs. h list")
+    title("Local error vs. h list for all four methods")
     xlabel("Time step sizes");
     ylabel("Error");
 
     % Find Global truncation error for Forward Euler, Explicit Midpoint
-    [global_h_list,global_fel_error_list,global_expmid_error_list, global_bel_error_list,global_impmid_error_list,global_analytical_list, tot_evals_fel,tot_evals_expmid, tot_evals_bel] = global_truncation_error(tspan, hspan, @rate_func01);
+    [global_h_list,global_fel_error_list,global_expmid_error_list, global_bel_error_list,global_impmid_error_list,global_analytical_list, tot_evals_fel,tot_evals_expmid, tot_evals_bel, tot_evals_impmid] = global_truncation_error(tspan, hspan, @rate_func01);
  
-    % Print out values for table of global truncation errors with step size
-    % (values of p). p values chosen are the first and last values of
-    % h_span, or -5 and -1.
-    fprintf("global analytical_difference: "); % STILL NEED TO DO THIS
-    global_analytical_list(1)
-    global_analytical_list(end)
-    fprintf("global_fel_error_list: ")
-    global_fel_error_list(1)
-    global_fel_error_list(end)
-    fprintf("global_expmid_error_list: ")
-    global_expmid_error_list(1)
-    global_expmid_error_list(end)
-
-    % Find line of best fit coefficients
-
+ 
+    % Find line of best fit coefficients and estimate p-values for test 1
     [g_fel_p,g_fel_k] = loglog_fit(global_h_list,global_fel_error_list);
     g_fel_y_data = g_fel_k.*((global_h_list.^g_fel_p));
+    fprintf("Global forward Euler p-value: ");
+    disp(g_fel_p);
 
     [g_expmid_p,g_expmid_k] = loglog_fit(global_h_list,global_expmid_error_list);
     g_expmid_y_data = g_expmid_k.*((global_h_list.^g_expmid_p));
-    
+    fprintf("Global backward Euler p-value: ");
+    disp(g_expmid_p);
+
+    [g_bel_p, g_bel_k] = loglog_fit(h_list,bel_error_list);
+    g_bel_y_data = g_bel_k.*((global_h_list.^g_bel_p));
+    fprintf("GLobal explicit midpoint p-value: ");
+    disp(g_bel_p);
+
+    [g_impmid_p, g_impmid_k] = loglog_fit(h_list,impmid_error_list);
+    g_impmid_y_data = g_impmid_k.*((global_h_list.^g_impmid_p));
+    fprintf("Global implicit Midpoint p-value: ");
+    disp(g_impmid_p);
+
 
     % Plot log scale graph of errors vs h_list (all the different step sizes used)
     figure;
@@ -154,8 +197,8 @@ function asst3()
     loglog(global_h_list,global_expmid_error_list,'go','MarkerFaceColor','g');
     loglog(global_h_list,g_expmid_y_data,'k','LineWidth',2); hold on; 
 
-    loglog(global_h_list,analytical_difference, 'bo', 'MarkerFaceColor', 'b'); hold off;
-    legend('Forward Euler','Fit line','Explicit Midpoint', '', 'Analytical Solution')
+    %loglog(global_h_list,analytical_difference, 'bo', 'MarkerFaceColor', 'b'); hold off;
+    legend('Forward Euler','Fit line','Explicit Midpoint', '')
     title("Global error vs. h list")
     xlabel("Time step sizes");
     ylabel("Error");
@@ -170,19 +213,37 @@ function asst3()
     loglog(tot_evals_expmid,global_expmid_error_list,'go','MarkerFaceColor','g');
     loglog(tot_evals_expmid,g_expmid_y_data,'k','LineWidth',2); hold on; 
 
-    loglog(tot_evals_bel,global_bel_error_list,'ro','MarkerFaceColor','r'); hold on;
-    loglog(tot_evals_bel,g_bel_y_data,'k','LineWidth',2);
+    %loglog(tot_evals_bel,global_bel_error_list,'ro','MarkerFaceColor','r'); hold on;
+    %loglog(tot_evals_bel,g_bel_y_data,'k','LineWidth',2);
 
-    loglog(tot_evals_impmid,global_impmid_error_list,'go','MarkerFaceColor','g');
-    loglog(tot_evals_impmid,g_impmid_y_data,'k','LineWidth',2); hold on; 
+    %loglog(tot_evals_impmid,global_impmid_error_list,'go','MarkerFaceColor','g');
+    %loglog(tot_evals_impmid,g_impmid_y_data,'k','LineWidth',2); hold on; 
 
-    loglog(tot_evals_fel,analytical_difference, 'bo', 'MarkerFaceColor', 'b'); hold off;
-    legend('Forward Euler','Fit line','Explicit Midpoint', '', 'Backward Euler','','Implicit Midpoint', '', 'Analytical Solution')
+    %loglog(tot_evals_fel,analytical_difference, 'bo', 'MarkerFaceColor', 'b'); hold off;
+    %legend('Forward Euler','Fit line','Explicit Midpoint', '', 'Backward Euler','','Implicit Midpoint', '')
+    legend('Forward Euler','Fit line','Explicit Midpoint')
     title('Global Error vs # of Calls')
     xlabel("# of calls");
     ylabel("Global error")
 
-    % Plot log scale graph of local truncation error scaling for all four
+    % Estimate p-values for global truncation error vs. rate function calls for test 1
+    [gcall_fel_p,~] = loglog_fit(tot_evals_fel, global_fel_error_list);
+    fprintf("Global/rate function calls forward Euler p-value: ");
+    disp(gcall_fel_p);
+
+    [gcall_expmid_p,~] = loglog_fit(tot_evals_expmid,global_expmid_error_list);
+    fprintf("Global/rate function calls backward Euler p-value: ");
+    disp(gcall_expmid_p);
+
+    [gcall_bel_p, ~] = loglog_fit(tot_evals_bel,bel_error_list);
+    fprintf("Global/rate function calls explicit midpoint p-value: ");
+    disp(gcall_bel_p);
+
+    [gcall_impmid_p, ~] = loglog_fit(tot_evals_impmid,impmid_error_list);
+    fprintf("Global/rate function calls implicit Midpoint p-value: ");
+    disp(gcall_impmid_p);
+
+    % Plot log scale graph of global truncation error scaling for all four
     % methods
     figure;
 
@@ -199,5 +260,6 @@ function asst3()
     xlabel("Time step sizes");
     ylabel("Error");
     hold off; 
+
 end
 
--- truncation_error.m
+++ truncation_error.m
@@ -13,30 +13,33 @@
 % 
 % end
 
-function [h_list,analytical_difference,fel_error_list,expmid_error_list] = truncation_error(t_ref, hspan, test_function)
+function [h_list,analytical_difference,fel_error_list,expmid_error_list, bel_error_list, impmid_error_list] = truncation_error(t_ref, hspan, test_function)
 
     h_list = logspace(hspan(1), hspan(2), hspan(3));
     x_approx_fel_list = []; % Forward Euler Local
     x_approx_expmid_list = []; % Explicit Midpoint
-    x_analytical_list = [];
+    x_analytical_list = []; % Analytical Solution
+    x_approx_bel_list = []; % Backward Euler Local
+    x_approx_impmid_list = []; % Implicit Midpoint
 
     XA = solution01(t_ref);
     for i = 1:(length(h_list)) % subtracting by two to make error_list the same size as t_list in asst3. Fix later.
         
         [x_approx_fel,~] = forward_euler_step(test_function,t_ref,XA,h_list(i));
         [x_approx_expmid,~] = explicit_midpoint_step(test_function,t_ref,XA,h_list(i));
-
+        [x_approx_bel, ~] = backward_euler_step(test_function,t_ref,XA,h_list(i));
+        [x_approx_impmid, ~] = implicit_midpoint_step(test_function,t_ref,XA,h_list(i));
 
         x_approx_fel_list(end+1) = x_approx_fel;
         x_approx_expmid_list(end+1) = x_approx_expmid;
         x_analytical_list(end+1) = solution01(t_ref+h_list(i));
+        x_approx_bel_list(end+1) = x_approx_bel;
+        x_approx_impmid_list(end+1) = x_approx_impmid;
     end
     analytical_difference = abs(x_analytical_list-XA); % |X(t + h) − X(t)|
     fel_error_list = abs(x_approx_fel_list - x_analytical_list);
     expmid_error_list = abs(x_approx_expmid_list - x_analytical_list);
-
-    % next step: plot as a function of hspan
-
-    
+    bel_error_list = abs(x_approx_bel_list - x_analytical_list);
+    impmid_error_list = abs(x_approx_impmid_list - x_analytical_list);
 
 end
